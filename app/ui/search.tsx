f1db446b305f90058b8c569c9382b16da29e34a2
Sean N <snewmanri@gmail.com>
2024-09-09 14:29:04 -0400

    update search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,23 +1,29 @@
 'use client';
- 
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
- 
+
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
- 
+
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-   
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`)
+    replace(`${pathname}?${params.toString()}`);
   }, 300);
+
+  return (
+    <div>
+      <input type="text" placeholder={placeholder} onChange={(e) => handleSearch(e.target.value)} />
+      <MagnifyingGlassIcon /> 
+    </div>
+  );
 }
